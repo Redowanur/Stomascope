7260aff89aeac1d2379812852a848f5bbb001dc2
Redowanur <rahmanlabib74@gmail.com>
2024-02-05 22:16:51 +0600

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-green-700 h-72">
-        <div className="w-100 justify-between flex items-center text-white mx-auto h-full">
-        <img src={require('./logo.png')} className="h-36" />
-            <div>
-                <h1 className="text-lg font-black mb-2">Social Media Links</h1>
-                <ul className="flex gap-10 text-3xl">
-                    <li><i class="fa-brands fa-facebook"></i><a href="https://www.facebook.com/" target="_"></a></li>
-                    <li><i class="fa-brands fa-twitter"></i></li>
-                    <li><i class="fa-brands fa-linkedin"></i></li>
-                    <li><i class="fa-brands fa-github"></i></li>
-                </ul>
+        <footer className="bg-green-700 h-56">
+            <div className="w-100 justify-between flex items-center text-white mx-auto h-full">
+                <img src={require('./logo.png')} className="h-36" />
+                <div>
+                    <h1 className="text-lg font-black mb-2">Social Media Links</h1>
+                    <ul className="flex gap-10 text-3xl">
+                        <li><a href="https://www.facebook.com/" target="_"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/" target="_"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="https://www.linkedin.com/" target="_"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="https://github.com/" target="_"><i class="fab fa-github"></i></a></li>
+                    </ul>
+
+                </div>
+                <div>
+                    <p>© 2024 Tahsin, Redowan, Nahin, Labib, Sajjad <br /> All rights reserved.</p>
+                </div>
             </div>
-            <div>
-                <p>© 2024 Tahsin, Redowan, Nahin, Labib, Sajjad <br/> All rights reserved.</p>
-            </div>
-        </div>
         </footer>
     )
 }
